f86b53ddddeec8c58417cf2a9f4184b0b51383db
VaishnaviKadham <118585128+VaishnaviKadham@users.noreply.github.com>
2024-01-26 10:24:34 +0530

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,43 @@
+	// Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
+
+    // hero type effect
+    const typed=document.querySelector('.typed');
+    if(typed){
+        let typed_strings= typed.getAttribute('data-typed-items');
+        typed_strings=typed_strings.split(',');
+        new Typed('.typed',{
+            strings : typed_strings,
+            loop:true,
+            typeSpeed:100,
+            backSpeed:50,
+            backDelay:2000
+        });
+    }
